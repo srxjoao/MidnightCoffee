e788812145b4222db024d8ca842596cf42e1ff6e
srxjoao <joao.barbosa6@estudante.ifms.edu.br>
2024-11-25 14:35:44 -0400

    aula /

--- cliente/src/pages/Registro.jsx
+++ cliente/src/pages/Registro.jsx
@@ -1,9 +1,46 @@
 import { useState } from "react";
-
-export default function Registrar() {
-
-
+  export default function Registrar() {
+    //aspa simples para texto
+  const [nome , setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const criaUsuario = async (event) => {
+    event.preventDefault();
+    try{
+      await fetch("http:localhost:3000/usuarios",{
+        method:'POST',
+        headers:{'Content-Type': 'Applicantion/json'},
+        //Passa a informação, passa o stringify para sair de texto para JSON
+        body: JSON.stringify({
+          nome: nome,
+          email: email
+        })
+      })
+    }catch{
+      alert("Deu erro baby");
+    }
+    alert("Dados salvos! Nome: ${nome}, Email: ${email}");
+  }
   return (
-        
+    <main>
+      <form onSubmit={criaUsuario}>
+        <label>
+          Nome
+          <input 
+          type="text"
+          value={nome}
+          onChange = {(event) => setNome(event.target.value)} required>
+          </input>
+          </label>
+        <label>
+          Email
+          <input
+          type="text"
+          value={email}
+          onChange = {(event) => setEmail(event.target.value)} required>
+          </input>
+        </label>
+        <button type="subimt" onClick={criaUsuario}>Criar Usuário</button>
+      </form>
+    </main>
   );
 }
