4ba7b8546af5b3875c0241931e9271c70ee84462
srxjoao <joao.barbosa6@estudante.ifms.edu.br>
2024-12-09 22:31:23 -0400

    inicio desenvolvimento projeto final

--- cliente/src/pages/Home.jsx
+++ cliente/src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
-
+import jsPDF from 'jspdf';
+import 'jspdf-autotable'
+import {Button} from "@mui/material"
+import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
 export default function Home() {
 
   const [usuarios, setUsuarios] = useState([]);
@@ -17,31 +20,47 @@ export default function Home() {
     buscarUsuario();
   }, [usuarios])
 
-  const apagar = async(id) =>{
+  const apagar = async(id) => {
     try{
-  
-   await  fetch("http://localhost:3000/usuarios"+ id, {
+    await  fetch("http://localhost:3000/usuarios/"+ id, {
     method: 'DELETE',
-   });
+    });
     }catch{
       alert("Desiste culpa do Marce Del Lino")
     }
    } 
-
+   //Código trbalho final para gerar PDF
+   const exportarPDF = () => {
+    const doc = new jsPDF();
+                  //Aki vai receber o nome do estado
+    const tabela = usuarios.map( usuario => [
+      usuario.id,
+      usuario.nome,
+      usuario.email
+    ]);
+    doc.text("Lista de Usuário", 10, 10);
+    doc.autoTable({
+    head:[["id","Nome", "E-mail"]],
+    body: tabela
+   });
+   doc.save("alunosIFMS")
+   }
    return (
+    <div>
+      <Button variant="contained" onClick={()=> exportarPDF()}> <PictureAsPdfIcon/> Gerar PDF</Button>
      <table>
       <tr>
         <td>Nome</td>
         <td>E-mail</td>
-      </tr>
-      <p>para listar usamos o map</p>
-      {usuarios.map((usuario) =>
-        <tr key={usuario.id}>
+       </tr>
+       {usuarios.map((usuario) =>
+         <tr key={usuario.id}>
           <td>{usuario.nome}</td>
           <td>{usuario.email}</td>
-          <td><button onClick={()=> apagar(usuario.id)}> X </button></td>
+          <td><button onClick={()=> apagar(usuario.id)}> Apagar  </button></td>
         </tr>
       )}
      </table>
+     </div>
   );
 }
